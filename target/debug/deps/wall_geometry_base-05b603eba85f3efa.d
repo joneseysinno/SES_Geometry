8df42509191776b142c5ae929faf7ebb4fff1851
Aaron Jones <abjones@joneseysinno.onmicrosoft.com>
2025-08-07 19:05:40 -0600

    added base

---
+++ target/debug/deps/wall_geometry_base-05b603eba85f3efa.d
@@ -0,0 +1,5 @@
+D:\Git\Rust\SES_Geometry\target\debug\deps\wall_geometry_base-05b603eba85f3efa.d: wall_geometry_base\src\lib.rs
+
+D:\Git\Rust\SES_Geometry\target\debug\deps\libwall_geometry_base-05b603eba85f3efa.rmeta: wall_geometry_base\src\lib.rs
+
+wall_geometry_base\src\lib.rs:
---
+++ target/debug/deps/wall_geometry_base-a962153b43a3dd10.d
@@ -0,0 +1,5 @@
+D:\Git\Rust\SES_Geometry\target\debug\deps\wall_geometry_base-a962153b43a3dd10.d: wall_geometry_base\src\lib.rs
+
+D:\Git\Rust\SES_Geometry\target\debug\deps\libwall_geometry_base-a962153b43a3dd10.rmeta: wall_geometry_base\src\lib.rs
+
+wall_geometry_base\src\lib.rs:
